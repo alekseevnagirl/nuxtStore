2468cce6e5b2df1c2538b3eac364a0a311d79830
alekseevnagirl <73663448+alekseevnagirl@users.noreply.github.com>
2024-10-22 14:42:23 +0400

    Add files via upload

--- index.vue
+++ index.vue
@@ -2,13 +2,14 @@
     <div class="catalog__wrapper">
         <Filter :filters="filters" 
             :filterData="filterData" 
-            class="catalog__filters">
+            class="catalog__filters"
+            @selectedFilter="selectedFilter">
         </Filter>
 
         <div class="catalog__items__wrapper">
             <p class="catalog__title">Catalog</p>
             <div class="catalog__items">
-                <div v-for="(product, productId) in productData" 
+                <div v-for="(product, productId) in setProductData(productData)" 
                     class="catalog__item" 
                     :key="itemId">
                     <Product :productData="product"></Product>
@@ -27,16 +28,36 @@
             return {
                 filters: ['Brand 1', 'Brand 2'],
                 filterData: [],
-                productData: []
+                productData: [],
+                filterId: 0,
+                productDataFiltered: []
             }
         },
         mounted() {
-            console.log(products)
-
             this.filterData =
                 { defaultFilter: 'All Brands', data: brands};
 
             this.productData = products;
+            this.productData.forEach((product) => {
+                brands.forEach((brand) => {
+                    if (product.brand === brand.id)
+                    product.brandName = brand.title;
+                })
+            })
+        },
+        methods: {
+            selectedFilter(id) {
+                this.filterId = id;
+            },
+            setProductData(productData) {
+                this.productDataFiltered = productData;
+                if (this.filterId !== 0) {
+                    this.productDataFiltered = productData.filter(product => {
+                        return product.brand === this.filterId;
+                    });
+                }
+                return this.productDataFiltered
+            }
         }
     }
 </script>
@@ -49,10 +70,10 @@
         padding: 20px 5%;
     }
     .catalog__filters {
-        flex: 1 1 20%;
+        flex: 1 1 15%;
     }
     .catalog__items__wrapper {
-        flex: 1 1 80%;
+        flex: 1 1 85%;
         display: block;
     }
     .catalog__items {
