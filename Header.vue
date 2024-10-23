d04886cbefed8d80980b2113600ec23eedd9c3ef
alekseevnagirl <73663448+alekseevnagirl@users.noreply.github.com>
2024-10-23 10:25:39 +0400

    Add files via upload

--- Header.vue
+++ Header.vue
@@ -3,9 +3,15 @@
         <NuxtLink to="/">
             <img src="/images/icons/logo.png" />
         </NuxtLink>
-        <NuxtLink to="/cart" v-if="route.path === '/'">
-            <img src="/images/icons/cart.png" class="header__icon"/>
-            <span>{{ quantity }}</span>
+
+        <NuxtLink to="/cart" 
+            v-if="route.path === '/'"
+            class="header__cart">
+            <img src="/images/icons/cart.png" 
+                class="header__cart__icon"/>
+            <span class="header__cart__counter">
+                {{ quantity }}
+            </span>
         </NuxtLink>
     </div>
 </template>
@@ -44,7 +50,23 @@
         margin: 20px 5%;
         justify-content: center;
     }
-    .header__icon {
+    .header__cart__icon {
         height: 34px;
+    }
+    .header__cart {
+        position: relative;
+        padding: 5px 20px 0 0;
+    }
+    .header__cart__counter {
+        position: absolute;
+        border: 1px solid #000;
+        border-radius: 20px;
+        padding: 1px 2px;
+        min-width: 20px;
+        text-align: center;
+        color: #000;
+        top: 0;
+        right: 0;
+        background-color: #fff;
     }
 </style>
