6413102fb62d2a1e6cb38d96aa0bd384ae9200bf
Natalia <kashitsyna.2001@mail.ru>
2024-10-25 09:49:03 +0400

    add level 1 and 2

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <v-app>
+      <NuxtPage></NuxtPage>
+    </v-app>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div style="display: flex; justify-content: space-around; align-content: center; flex-wrap: wrap; height: 100vh;">
+        <v-dialog max-width="500">
+  <template v-slot:activator="{ props: activatorProps }">
+    <v-btn
+      v-bind="activatorProps"
+      color="surface-variant"
+      text="Open"
+      variant="flat"
+    ></v-btn>
+  </template>
+
+  <template v-slot:default="{ isActive }">
+    <v-card title="Dialog">
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close Dialog"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
+    </div>
+</template>
