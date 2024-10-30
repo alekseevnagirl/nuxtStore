c044e51adfb2e3ddf51ee13d87d52d1bbe9247b8
Natalia <kashitsyna.2001@mail.ru>
2024-10-30 11:51:10 +0400

    fix with marks

--- components/ConfigurableProduct.vue
+++ components/ConfigurableProduct.vue
@@ -7,12 +7,16 @@
                 <div class="configurableProduct__items__wrapper">
                     <div v-for="(item, id) in option.values" 
                         :key="id">
-                        <div class="configurableProduct__item"
-                            :id="`${productData.id}-${item.value_index}`"
+                        <div :class="`configurableProduct__item-${productData.id}`"
+                            :id="`${productData.id}-${option.attribute_code}-${item.value_index}`"
                             :style="isColor(option, item)"
                             @click="chooseOption(option, item)">
-                            <p v-if="option.attribute_code === 'size'">
+                            <p v-if="option.attribute_code === 'size'"
+                                class="configurableProduct__item__label">
                                 {{ item.label }}
+                            </p>
+                            <p class="configurableProduct__item__none">
+                                ×
                             </p>
                         </div>
                     </div>
@@ -38,12 +42,16 @@
         watch: {
             unavailableSizes() {
                 this.unavailableSizes.forEach((id) => {
-                    document.getElementById(id).style.display = 'none';
+                    let div = document.getElementById(id);
+                    div.style.pointerEvents = 'none';
+                    div.querySelector('.configurableProduct__item__none').style.display = 'block';
                 })
             },
             unavailableColors() {
                 this.unavailableColors.forEach((id) => {
-                    document.getElementById(id).style.display = 'none';
+                    let div = document.getElementById(id);
+                    div.style.pointerEvents = 'none';
+                    div.querySelector('.configurableProduct__item__none').style.display = 'block';
                 })
             }
         },
@@ -61,33 +69,36 @@
             },
             chooseOption(option, item) {
                 // задаем айди, приводим к дефолтным значениям и стилям
-                let id = this.productData.id + '-' + item.value_index;
+                let id = this.productData.id + '-' + option.attribute_code + '-' + item.value_index;
                 this.availableColors = [];
                 this.availableSizes = [];
                 this.unavailableColors = [];
                 this.unavailableSizes = [];
-                let elements = document.querySelectorAll('.configurableProduct__item');
-                elements.forEach(elem => {
-                    elem.style.display = 'block';
-                    elem.style.border = '1px solid #000';
-                });
 
                 // если уже выбран цвет/размер с таким айди, то дефолтим
                 if (this.selectedColor === id || this.selectedSize === id) {
-                    this.selectedColor = null;
-                    this.selectedSize = null;
-                    this.imageSrc = '';
-                } else { 
+                    if (this.selectedColor === id) {
+                        this.selectedColor = null;
+                        this.imageSrc = '';
+                    }
+                    if (this.selectedSize === id) this.selectedSize = null;
+
+                    let element = document.getElementById(id);
+                    element.style.border = '1px solid #000';
+                    element.style.pointerEvents = 'auto';
+                    element.querySelector('.configurableProduct__item__none').style.display = 'none';
+                } 
+                else { 
                     // если этот цвет/размер еще не выбран
                     // задаем новые значения выбранных цвета и размера
                     if (option.attribute_code === 'color') { 
-                        this.selectedColor = id;debugger
+                        this.selectedColor = id;
 
                         // меняем картинку товара в зависимости от цвета
                         let selectedVariant = this.productData.variants.find((variant) => {
                             return variant.attributes.some((attribute) => {
                                 return attribute.code === 'color' && 
-                                    this.selectedColor === this.productData.id + '-' + attribute.value_index;
+                                    this.selectedColor === this.productData.id + '-' + option.attribute_code + '-' + attribute.value_index;
                             });
                         });
 
@@ -108,13 +119,13 @@
                         isColor = variant.attributes.find((attribute) => {
                             return attribute.code === 'color' 
                                 && this.selectedColor !== null 
-                                && attribute.value_index.toString() === this.selectedColor.split('-')[1];
+                                && attribute.value_index.toString() === this.selectedColor.split('-')[2];
                         }) !== undefined;
 
                         if (isColor) {
                             variant.attributes.forEach((attribute) => {
                                 if (attribute.code === 'size') {
-                                    let size = this.productData.id + '-' + attribute.value_index;
+                                    let size = this.productData.id + '-' + attribute.code + '-' + attribute.value_index;
                                     this.availableSizes = this.availableSizes.concat(size);
                                 }
                             });
@@ -123,13 +134,13 @@
                         isSize = variant.attributes.find((attribute) => {
                             return attribute.code === 'size' 
                                 && this.selectedSize !== null
-                                && attribute.value_index.toString() === this.selectedSize.split('-')[1]
+                                && attribute.value_index.toString() === this.selectedSize.split('-')[2]
                         }) !== undefined;
 
                         if (isSize) {
                             variant.attributes.forEach((attribute) => {
                                 if (attribute.code === 'color') { 
-                                    let color = this.productData.id + '-' + attribute.value_index;
+                                    let color = this.productData.id + '-' + attribute.code + '-' + attribute.value_index;
                                     this.availableColors = this.availableColors.concat(color);
                                 }
                             })
@@ -141,7 +152,7 @@
                         this.productData.configurable_options.forEach((option) => {
                             if (option.attribute_code === 'size') {
                                 option.values.forEach((value) => {
-                                    let optionSize = this.productData.id + '-' + value.value_index
+                                    let optionSize = this.productData.id + '-' + option.attribute_code + '-' + value.value_index
                                     if (!this.availableSizes.includes(optionSize)) 
                                         this.unavailableSizes = this.unavailableSizes.concat(optionSize);
                                 })
@@ -155,7 +166,7 @@
                         this.productData.configurable_options.forEach((option) => {
                             if (option.attribute_code === 'color') {
                                 option.values.forEach((value) => {
-                                    let optionColor = this.productData.id + '-' + value.value_index
+                                    let optionColor = this.productData.id + '-' + option.attribute_code + '-' + value.value_index
                                     if (!this.availableColors.includes(optionColor)) 
                                         this.unavailableColors = this.unavailableColors.concat(optionColor);
                                 })
@@ -182,14 +193,25 @@
         display: flex;
         gap: 5px;
     }
-    .configurableProduct__item {
+    [class^="configurableProduct__item-"] {
         width: 30px;
         height: 20px;
         border: 1px solid #000;
         text-align: center;
         font-size: 14px;
+        position: relative;
     }
-    .configurableProduct__item:hover {
+    [class^="configurableProduct__item-"]:hover {
         cursor: pointer;
+    }
+    .configurableProduct__item__label {
+        position: absolute;
+        width: 100%;
+        text-align: center;
+    }
+    .configurableProduct__item__none {
+        color: #8B0000;
+        transform: scale(3);
+        display: none;
     }
 </style>
